7cfd47a95cffddde2ca29a2cd8969240258946dd
tommygod3 <tommygod1@live.com>
2018-11-29 09:41:26 +0000

    Improved formatting of inputs

--- downloads/forms.py
+++ downloads/forms.py
@@ -7,4 +7,4 @@ class FilterForm(forms.Form):
     user = forms.CharField(max_length=100, required=False, label="User:", widget=forms.TextInput(attrs={"class": "form-control", "placeholder":""}))
     dataset = forms.CharField(max_length=100, required=False, label="Dataset:", widget=forms.TextInput(attrs={"class": "form-control", "placeholder":""}))
     method = forms.CharField(max_length=100, required=False, label="Method:", widget=forms.TextInput(attrs={"class": "form-control", "placeholder":""}))
-    anon = forms.CharField(max_length=100, required=False, label="User type:", widget=forms.TextInput(attrs={"class": "form-control", "placeholder":""}))
+    anon = forms.ChoiceField(choices=[("all","All"),("only-anon","Only Anonymous Users"),("non-anon","Registered Users")], required=False, label="User type:", widget=forms.Select(attrs={"class": "form-control", "placeholder":""}))
