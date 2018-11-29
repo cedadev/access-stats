c57b402fc0946162f3478b5ea671267ea7cf65ec
tommygod3 <tommygod1@live.com>
2018-11-29 14:49:29 +0000

    Added tab functionality and conversion dropdown

--- downloads/urls.py
+++ downloads/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import IndexView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<analysis_method>/', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index'),
 ]
--- downloads/views.py
+++ downloads/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.views.generic import TemplateView
 
 from .forms import FilterForm
 
-def index(request):
-    if request.method == "GET":
+class IndexView(TemplateView):
+    template_name = "downloads/index.html"
+    analysis_methods = ["methods","timeline","dataset","users","user","trace"]
+
+    def get(self, request, analysis_method="methods"):
+        if analysis_method not in self.analysis_methods:
+            return HttpResponseNotFound('<h1>404 - Not found</h1>')
         form = FilterForm(request.GET)
-    return render(request, "downloads/index.html", {'form': form})
+        return render(request, self.template_name, {'form': form, 'analysis_method':analysis_method})
