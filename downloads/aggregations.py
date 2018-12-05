8efc367f1ead3ae2ce9eba9ff0814a4748f5d08e
tommygod3 <tommygod1@live.com>
2018-12-05 17:09:11 +0000

    Query maker and aggreagtions maker finished for all analysis methods except user breakdown, per user and per dataset views are composite aggs which reduce load on es

--- downloads/aggregations.py
+++ downloads/aggregations.py
@@ -24,20 +24,11 @@ class AggregationsMaker:
                         }
                     },
                     "group_by_first_nested": {
-                        "terms": {
-                            "field": "user.keyword"
-                        },
                         "aggs": {
                             "group_by_second_nested": {
-                                "date_histogram": {
-                                    "field": "datetime",
-                                    "interval": "day"
-                                },
                                 "aggs": {
                                     "activity_days": {
-                                        "cardinality": {
-                                            "field": "dataset.keyword"
-                                        }
+                                        "cardinality": {}
                                     }
                                 }
                             },
@@ -57,28 +48,96 @@ class AggregationsMaker:
             }
         }
 
-    def get_aggs(self, analysis_method):
+    def get_aggs(self, analysis_method, after_key=None):
         self.generated_aggs = self.constant_aggs
-        self._add_group_by(analysis_method)
+        self._add_group_by(analysis_method, after_key)
         self._add_nested_aggs(analysis_method)
         return self.generated_aggs
 
     def _add_nested_aggs(self, analysis_method):
         # Correctly add sub aggs so analysis_method not in them
-        pass
+        if analysis_method == "methods":
+            self._add_nested_aggs_methods()
+        if analysis_method == "timeline":
+            self._add_nested_aggs_timeline()
+        if analysis_method == "dataset":
+            self._add_nested_aggs_dataset()
+        if analysis_method == "users":
+            self._add_nested_aggs_users()
 
-    def _add_group_by(self, analysis_method):
+    def _add_nested_aggs_methods(self):
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["terms"] = {}
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["terms"]["field"] = "user.keyword"
+
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["date_histogram"] = {}
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["date_histogram"]["field"]  = "datetime"
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["date_histogram"]["interval"]  = "day"
+
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["aggs"]["activity_days"]["cardinality"]["field"] = "dataset.keyword"
+
+    def _add_nested_aggs_timeline(self):
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["terms"] = {}
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["terms"]["field"] = "user.keyword"
+
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["terms"] = {}
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["terms"]["field"] = "method.keyword"
+
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["aggs"]["activity_days"]["cardinality"]["field"] = "dataset.keyword"
+    
+    def _add_nested_aggs_dataset(self):
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["terms"] = {}
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["terms"]["field"] = "user.keyword"
+
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["date_histogram"] = {}
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["date_histogram"]["field"]  = "datetime"
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["date_histogram"]["interval"]  = "day"
+
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["aggs"]["activity_days"]["cardinality"]["field"] = "method.keyword"
+
+    def _add_nested_aggs_users(self):
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["terms"] = {}
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["terms"]["field"] = "method.keyword"
+
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["date_histogram"] = {}
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["date_histogram"]["field"]  = "datetime"
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["date_histogram"]["interval"]  = "day"
+
+        self.generated_aggs["group_by"]["aggs"]["group_by_first_nested"]["aggs"]["group_by_second_nested"]["aggs"]["activity_days"]["cardinality"]["field"] = "dataset.keyword"
+
+    def _add_group_by(self, analysis_method, after_key=None):
         # Add correct group_by dict dependent on analysis_method (time produces a histogram)
         if analysis_method == "methods":
             self.generated_aggs["group_by"]["terms"] = {}
             self.generated_aggs["group_by"]["terms"]["field"] = "method.keyword"
+            self.generated_aggs["group_by"]["terms"]["size"] = "30"
         if analysis_method == "timeline":
             self.generated_aggs["group_by"]["date_histogram"] = {}
             self.generated_aggs["group_by"]["date_histogram"]["field"] =  "datetime"
             self.generated_aggs["group_by"]["date_histogram"]["interval"] =  "month"
         if analysis_method == "dataset":
-            self.generated_aggs["group_by"]["terms"] = {}
-            self.generated_aggs["group_by"]["terms"]["field"] = "dataset.keyword"
-        if analysis_method == "user":
-            self.generated_aggs["group_by"]["terms"] = {}
-            self.generated_aggs["group_by"]["terms"]["field"] = "user.keyword"
+            self.generated_aggs["group_by"]["composite"] = {}
+            self.generated_aggs["group_by"]["composite"]["size"] = 1000
+            if after_key:
+                self.generated_aggs["group_by"]["composite"]["after"] = after_key
+            self.generated_aggs["group_by"]["composite"]["sources"] = []
+            self.generated_aggs["group_by"]["composite"]["sources"].append({
+                "dataset": {
+                    "terms": {
+                        "field": "dataset.keyword"
+                    }
+                }
+            })
+            
+        if analysis_method == "users":
+            self.generated_aggs["group_by"]["composite"] = {}
+            self.generated_aggs["group_by"]["composite"]["size"] = 1000
+            if after_key:
+                self.generated_aggs["group_by"]["composite"]["after"] = after_key
+            self.generated_aggs["group_by"]["composite"]["sources"] = []
+            self.generated_aggs["group_by"]["composite"]["sources"].append({
+                "user": {
+                    "terms": {
+                        "field": "user.keyword"
+                    }
+                }
+            })
