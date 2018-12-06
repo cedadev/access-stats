55beacb7855cae02499bbdcd170baaefb5ccec04
tommygod3 <tommygod1@live.com>
2018-12-06 15:56:24 +0000

    Tables implemented for all 5 working views

--- downloads/static/downloads/js/templates.js
+++ downloads/static/downloads/js/templates.js
@@ -2,5 +2,10 @@ var templates = {
     methodsTableBody: '<tr><th scope="row">{{method}}</th><td>{{users}}</td><td>{{datasets}}</td><td>{{accesses}}</td><td>{{size}}</td><td>{{activitydays}}</td></tr>',
     methodsTableFooter: '<tr><th scope="row">{{totals}}</th><th>{{users}}</th><th>{{datasets}}</th><th>{{accesses}}</th><th>{{size}}</th><th>{{activitydays}}</th></tr>',
     timelineTableBody: '<tr><th scope="row">{{month}}</th><td>{{users}}</td><td>{{methods}}</td><td>{{datasets}}</td><td>{{accesses}}</td><td>{{size}}</td><td>{{activitydays}}</td></tr>',
-    timelineTableFooter: '<tr><th scope="row">{{totals}}</th><th>{{users}}</th><th>{{methods}}</th><th>{{datasets}}</th><th>{{accesses}}</th><th>{{size}}</th><th>{{activitydays}}</th></tr>'
+    timelineTableFooter: '<tr><th scope="row">{{totals}}</th><th>{{users}}</th><th>{{methods}}</th><th>{{datasets}}</th><th>{{accesses}}</th><th>{{size}}</th><th>{{activitydays}}</th></tr>',
+    datasetTableBody: '<tr><th scope="row">{{dataset}}</th><td>{{users}}</td><td>{{methods}}</td><td>{{accesses}}</td><td>{{size}}</td><td>{{activitydays}}</td></tr>',
+    datasetTableFooter: '<tr><th scope="row">{{totals}}</th><th>{{users}}</th><th>{{methods}}</th><th>{{accesses}}</th><th>{{size}}</th><th>{{activitydays}}</th></tr>',
+    usersTableBody: '<tr><th scope="row">{{user}}</th><td>{{methods}}</td><td>{{datasets}}</td><td>{{accesses}}</td><td>{{size}}</td><td>{{activitydays}}</td></tr>',
+    usersTableFooter: '<tr><th scope="row">{{totals}}</th><th>{{methods}}</th><th>{{datasets}}</th><th>{{accesses}}</th><th>{{size}}</th><th>{{activitydays}}</th></tr>',
+    traceTableBody: '<tr><td><pre>{{log}}</pre></td></tr>'
 }
