3fbca6619eca4df9088184b5983033394f79c9a2
tommygod3 <tommygod1@live.com>
2018-12-06 15:10:48 +0000

    Javascript now loads methods and timeline tables, favicon working

---
+++ downloads/static/downloads/js/templates.js
@@ -0,0 +1,6 @@
+var templates = {
+    methodsTableBody: '<tr><th scope="row">{{method}}</th><td>{{users}}</td><td>{{datasets}}</td><td>{{accesses}}</td><td>{{size}}</td><td>{{activitydays}}</td></tr>',
+    methodsTableFooter: '<tr><th scope="row">{{totals}}</th><th>{{users}}</th><th>{{datasets}}</th><th>{{accesses}}</th><th>{{size}}</th><th>{{activitydays}}</th></tr>',
+    timelineTableBody: '<tr><th scope="row">{{month}}</th><td>{{users}}</td><td>{{methods}}</td><td>{{datasets}}</td><td>{{accesses}}</td><td>{{size}}</td><td>{{activitydays}}</td></tr>',
+    timelineTableFooter: '<tr><th scope="row">{{totals}}</th><th>{{users}}</th><th>{{methods}}</th><th>{{datasets}}</th><th>{{accesses}}</th><th>{{size}}</th><th>{{activitydays}}</th></tr>'
+}
