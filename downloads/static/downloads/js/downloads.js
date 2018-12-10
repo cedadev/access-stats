40087e88b0ddcdc6b4d3d8b0dc5ab9a3cef61161
tommygod3 <tommygod1@live.com>
2018-12-10 12:29:00 +0000

    Timeline page finished and updating of chart per view added

--- downloads/static/downloads/js/downloads.js
+++ downloads/static/downloads/js/downloads.js
@@ -51,181 +51,148 @@ function renderMethodsPage(data)
     $("#methodsTableFooter").html(html);
 }
 
-
-
 function renderTimelinePage(data)
 {
-    var months = [];
-    var users = [];
-    var methods = [];
-    var datasets = [];
-    var accesses = [];
-    var size = [];
-    var activitydays = [];
+    var dataDict = {
+        months: [],
+        users: [],
+        methods: [],
+        datasets: [],
+        accesses: [],
+        size: [],
+        activitydays: []
+    }
+
 
     var html;
     for (var month in data.results)
     {
-        months.push(formatDate(month));
-        users.push(data.results[month].users);
-        methods.push(data.results[month].methods);
-        datasets.push(data.results[month].datasets);
-        accesses.push(data.results[month].accesses);
-        size.push(data.results[month].size);
-        activitydays.push(data.results[month].activitydays);
+        dataDict.months.push(formatDate(month));
+        dataDict.users.push(data.results[month].users);
+        dataDict.methods.push(data.results[month].methods);
+        dataDict.datasets.push(data.results[month].datasets);
+        dataDict.accesses.push(data.results[month].accesses);
+        dataDict.size.push(data.results[month].size);
+        dataDict.activitydays.push(data.results[month].activitydays);
         html += Mustache.render(templates.timelineTableBody, {month:formatDate(month),users:data.results[month].users,methods:data.results[month].methods,datasets:data.results[month].datasets,accesses:data.results[month].accesses,size:formatBytes(data.results[month].size),activitydays:data.results[month].activitydays});
     }
     $("#timelineTableBody").html(html);
     html = Mustache.render(templates.timelineTableFooter, {totals:"Totals",users:data.totals.users,methods:data.totals.methods,datasets:data.totals.datasets,accesses:data.totals.accesses,size:formatBytes(data.totals.size),activitydays:data.totals.activitydays});
     $("#timelineTableFooter").html(html);
 
-    timelineUsers = makeTableUsers(months, users);
+    timelineChart = makeChart(dataDict);
 
-    var timelineChartMethods = document.getElementById("timelineChartMethods").getContext('2d');
-    var timelineMethods = new Chart(timelineChartMethods, {
-        type: 'line',
-        data: {
-            labels: months,
-            datasets: [{
-                label: '# of methods',
-                data: methods,
-                fill: false,
-                borderColor: "#00628d",
-                backgroundColor: "#00628d",
-                pointBackgroundColor: "#00628d",
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: false,
-            legend: {
-                display: false
-            }
-        }
-    
-    });
-    var timelineChartDatasets = document.getElementById("timelineChartDatasets").getContext('2d');
-    var timelineDatasets = new Chart(timelineChartDatasets, {
-        type: 'line',
-        data: {
-            labels: months,
-            datasets: [{
-                label: '# of datasets',
-                data: datasets,
-                fill: false,
-                borderColor: "#00628d",
-                backgroundColor: "#00628d",
-                pointBackgroundColor: "#00628d",
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: false,
-            legend: {
-                display: false
-            }
-        }
-    
-    });
-    var timelineChartAccesses = document.getElementById("timelineChartAccesses").getContext('2d');
-    var timelineAccesses = new Chart(timelineChartAccesses, {
-        type: 'line',
-        data: {
-            labels: months,
-            datasets: [{
-                label: '# of accesses',
-                data: accesses,
-                fill: false,
-                borderColor: "#00628d",
-                backgroundColor: "#00628d",
-                pointBackgroundColor: "#00628d",
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: false,
-            legend: {
-                display: false
-            }
-        }
-    
-    });
-    var timelineChartSize = document.getElementById("timelineChartSize").getContext('2d');
-    var timelineSize = new Chart(timelineChartSize, {
-        type: 'line',
-        data: {
-            labels: months,
-            datasets: [{
-                label: 'size',
-                data: size,
-                fill: false,
-                borderColor: "#00628d",
-                backgroundColor: "#00628d",
-                pointBackgroundColor: "#00628d",
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: false,
-            legend: {
-                display: false
-            }
-        }
-    
-    });
-    var timelineChartActivitydays = document.getElementById("timelineChartActivitydays").getContext('2d');
-    var timelineActivityDays = new Chart(timelineChartActivitydays, {
-        type: 'line',
-        data: {
-            labels: months,
-            datasets: [{
-                label: 'activity days',
-                data: activitydays,
-                fill: false,
-                borderColor: "#00628d",
-                backgroundColor: "#00628d",
-                pointBackgroundColor: "#00628d",
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: false,
-            legend: {
-                display: false
-            }
-        }
-    
-    });
-
-    $('.nav-tabs a').on('shown.bs.tab', function(event){
-        var x = $(event.target).text();         
-        if(x == "Users")
+    var activeTab = "timelineTabUsers";
+    timelineTabs = ["timelineTabUsers","timelineTabMethods","timelineTabDatasets","timelineTabAccesses","timelineTabSize","timelineTabActivitydays"]
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        if (timelineTabs.includes(e.target.id))
         {
-            timelineUsers.destroy();
-            makeTableUsers(months, users);
+            activeTab = e.target.id;
         }
-        console.log($(event.target));
-    }); 
-
-
+        timelineChart = updateChart(timelineChart, activeTab, dataDict);
+    })
 }
 
-function makeTableUsers(months, users)
+function updateChart(chart, activeTab, dataDict)
 {
-    var timelineChartUsers = document.getElementById("timelineChartUsers").getContext('2d');
+    chart.destroy();
+    chart = makeChart(dataDict);
+    if(activeTab == "timelineTabUsers")
+    {
+        chart.data.datasets[0].hidden = false;
+    }
+    if(activeTab == "timelineTabMethods")
+    {
+        chart.data.datasets[1].hidden = false;
+    }
+    if(activeTab == "timelineTabDatasets")
+    {
+        chart.data.datasets[2].hidden = false;
+    }
+    if(activeTab == "timelineTabAccesses")
+    {
+        chart.data.datasets[3].hidden = false;
+    }
+    if(activeTab == "timelineTabSize")
+    {
+        chart.data.datasets[4].hidden = false;
+    }
+    if(activeTab == "timelineTabActivitydays")
+    {
+        chart.data.datasets[5].hidden = false;
+    }
+    chart.update();
+    return chart;
+}
+
+function makeChart(dataDict)
+{
+    var timelineChartUsers = document.getElementById("timelineChart").getContext('2d');
     var timelineUsers = new Chart(timelineChartUsers, {
         type: 'line',
         data: {
-            labels: months,
+            labels: dataDict.months,
             datasets: [{
                 label: '# of users',
-                data: users,
+                data: dataDict.users,
                 fill: false,
                 borderColor: "#00628d",
                 backgroundColor: "#00628d",
                 pointBackgroundColor: "#00628d",
-                borderWidth: 1
-            }]
+                borderWidth: 1,
+                hidden: true
+            },
+            {
+                label: '# of methods',
+                data: dataDict.methods,
+                fill: false,
+                borderColor: "#00628d",
+                backgroundColor: "#00628d",
+                pointBackgroundColor: "#00628d",
+                borderWidth: 1,
+                hidden: true
+            },
+            {
+                label: '# of datasets',
+                data: dataDict.datasets,
+                fill: false,
+                borderColor: "#00628d",
+                backgroundColor: "#00628d",
+                pointBackgroundColor: "#00628d",
+                borderWidth: 1,
+                hidden: true
+            },
+            {
+                label: '# of accesses',
+                data: dataDict.accesses,
+                fill: false,
+                borderColor: "#00628d",
+                backgroundColor: "#00628d",
+                pointBackgroundColor: "#00628d",
+                borderWidth: 1,
+                hidden: true
+            },
+            {
+                label: 'size',
+                data: dataDict.size,
+                fill: false,
+                borderColor: "#00628d",
+                backgroundColor: "#00628d",
+                pointBackgroundColor: "#00628d",
+                borderWidth: 1,
+                hidden: true
+            },
+            {
+                label: '# of activity days',
+                data: dataDict.activitydays,
+                fill: false,
+                borderColor: "#00628d",
+                backgroundColor: "#00628d",
+                pointBackgroundColor: "#00628d",
+                borderWidth: 1,
+                hidden: true
+            }
+        ]
         },
         options: {
             responsive: false,
