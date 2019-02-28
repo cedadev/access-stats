c253dd7219ad8d896137481c735de511cd895685
tommygod3 <tommygod1@live.com>
2019-02-28 12:06:23 +0000

    Renamed pages in urls.py to be specific fixing a bug where wrong page was chosen

--- deposits/urls.py
+++ deposits/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from deposits.views import IndexView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='deposits'),
 ]
