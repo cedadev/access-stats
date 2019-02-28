a703c560f7118b2e25187e7d854da99661ced75d
tommygod3 <tommygod1@live.com>
2019-02-28 12:06:23 +0000

    Renamed pages in urls.py to be specific fixing a bug where wrong page was chosen

--- downloads/urls.py
+++ downloads/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from downloads.views import IndexView, JsonView, TxtView, CsvView, XlsxView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='downloads'),
     path('json/<analysis_method>', JsonView.as_view(), name='json'),
     path('txt/<analysis_method>', TxtView.as_view(), name='txt'),
     path('csv/<analysis_method>', CsvView.as_view(), name='csv'),
