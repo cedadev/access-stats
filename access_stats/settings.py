72e1b833d3e3b8f8409ef69cc431c8b1074d12de
tommygod3 <tommygod1@live.com>
2019-03-05 11:24:38 +0000

    Front page of access stats finished and some work on deposits page though not functional

--- access_stats/settings.py
+++ access_stats/settings.py
@@ -84,6 +84,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'access_stats/static')]
+
 STATIC_URL = '/static/'
 
 # Sensitive settings imported from local settings
