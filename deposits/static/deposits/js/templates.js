17ebe389a9f818503342e2e5d5613e860d936513
tommygod3 <tommygod1@live.com>
2019-03-13 12:12:30 +0000

    Changed tables to DataTables with sorting

--- deposits/static/deposits/js/templates.js
+++ deposits/static/deposits/js/templates.js
@@ -1,8 +1,5 @@
 var templates = {
-    timelineTableBody: '<tr><th scope="row">{{day}}</th><td>{{size}}</td><td>{{datasets}}</td><td>{{deposits}}</td><td>{{directories}}</td><td>{{symlinks}}</td><td>{{removedDirs}}</td><td>{{removedFiles}}</td></tr>',
-    timelineTableTotals: '<tr><th scope="row">{{totals}}</th><th>{{size}}</th><th>{{datasets}}</th><th>{{deposits}}</th><th>{{directories}}</th><th>{{symlinks}}</th><th>{{removedDirs}}</th><th>{{removedFiles}}</th></tr>',
-    datasetTableBody: '<tr><th scope="row">{{name}}</th><td>{{size}}</td><td>{{datasets}}</td><td>{{deposits}}</td><td>{{directories}}</td><td>{{symlinks}}</td><td>{{removedDirs}}</td><td>{{removedFiles}}</td></tr>',
-    datasetTableTotals: '<tr><th scope="row">{{totals}}</th><th>{{size}}</th><th>{{datasets}}</th><th>{{deposits}}</th><th>{{directories}}</th><th>{{symlinks}}</th><th>{{removedDirs}}</th><th>{{removedFiles}}</th></tr>',
+    tableTotals: '<tr><th scope="row">{{totals}}</th><th>{{size}}</th><th>{{datasets}}</th><th>{{deposits}}</th><th>{{directories}}</th><th>{{symlinks}}</th><th>{{removedDirs}}</th><th>{{removedFiles}}</th></tr>',
     traceTableBody: '{{log}}\n',
     loadingMessage: '<div class="alert alert-light text-center text-dark">Loading...</div>',
     warningMessage: '<div class="alert alert-danger text-center">Too many {{analysis_method}}. Showing 500 of {{total}}. <a href="javascript:{{allFunction}}">Show All</a></div>',
