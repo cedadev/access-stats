4b38950188c3928ee6fae9d24eb83da550af1d29
tommygod3 <tommygod1@live.com>
2019-03-19 09:59:06 +0000

    Made file reading for settings independent of working directory

--- common/json_maker.py
+++ common/json_maker.py
@@ -1,4 +1,6 @@
 import yaml
+from pathlib import Path
+
 from elasticsearch import Elasticsearch
 
 from common.query_builder_factory import QueryBuilderFactory
@@ -8,7 +10,12 @@ class JsonMaker:
         self.filters = filters
         self.analysis_method = analysis_method
 
-        self.load_settings("access_stats/settings.yml")
+        parent_dir = (Path(__file__).parent).parent
+        settings_file = parent_dir.joinpath("access_stats/settings.yml")
+        if not settings_file.exists():
+            raise FileNotFoundError(f"{settings_file} not found")
+
+        self.load_settings(settings_file)
     
         self.host = "https://jasmin-es1.ceda.ac.uk"
         
