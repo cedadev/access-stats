b2843c7304a0fce82cfa00933b9ad5936b142f18
tommygod3 <tommygod1@live.com>
2019-06-13 15:13:34 +0000

    Removed redundant function and tidied activity_days dict in json

--- common/users_json.py
+++ common/users_json.py
@@ -5,7 +5,7 @@ class UsersJson(JsonMaker):
         return "Summary of downloads from CEDA archive by user"
 
     def _populate_json(self):
-        self.setup_activity_days_dict("user")
+        activity_days_dict = self.get_activity_days_dict("user")
         response = self.get_elasticsearch_response(after_key = 0)
         self.generated_json["totals"] = {}
         self.generated_json["totals"]["users"] = response["aggregations"]["grand_total_users"]["value"]
@@ -28,7 +28,7 @@ class UsersJson(JsonMaker):
                 self.generated_json["results"][result["key"]["user"]]["datasets"] = result["number_of_datasets"]["value"]
                 self.generated_json["results"][result["key"]["user"]]["accesses"] = result["doc_count"]
                 self.generated_json["results"][result["key"]["user"]]["size"] = result["total_size"]["value"]
-                activity_days = self.activity_days_dict.get(result["key"]["user"], 0)
+                activity_days = activity_days_dict.get(result["key"]["user"], 0)
                 self.generated_json["results"][result["key"]["user"]]["activitydays"] = activity_days
                 self.generated_json["totals"]["activitydays"] += activity_days
             after_key = response["aggregations"]["group_by"]["after_key"]
