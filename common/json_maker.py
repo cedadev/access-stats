dc996e05db8f26c994a1825f2acc336356d1be57
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-09-29 17:05:00 +0100

    Made changes to work with es7. closes #7

--- common/json_maker.py
+++ common/json_maker.py
@@ -1,11 +1,15 @@
 import yaml
 from pathlib import Path
 
-from elasticsearch import Elasticsearch
+from ceda_elasticsearch_tools.elasticsearch import CEDAElasticsearchClient
 
 from common.query_builder_factory import QueryBuilderFactory
 
+
 class JsonMaker:
+
+    INDEX_KEY = "rollup"
+
     def __init__(self, filters, analysis_method):
         self.filters = filters
         self.analysis_method = analysis_method
@@ -17,14 +21,16 @@ class JsonMaker:
 
         self.load_settings(settings_file)
         
-        self.es = Elasticsearch(
+        self.es = CEDAElasticsearchClient(
             [self.settings["host"]],
-            http_auth=(self.settings["user"], self.settings["password"]),
+            headers={"x-api-key": self.settings["es_api_key"]},
+            ca_certs=None,
             timeout=60
         )
-    
+
+    @property
     def index(self):
-        return "rollup"
+        return self.settings["index"][self.INDEX_KEY]
 
     def load_settings(self, file_name):
         with open(file_name) as secrets:
@@ -33,11 +39,25 @@ class JsonMaker:
             except yaml.YAMLError as e:
                 raise RuntimeError(f"settings.yml file incorrect yaml: {e}")
 
-    def get_elasticsearch_response(self, after_key = None, deposits = False):
-        query = QueryBuilderFactory(deposits = deposits).get(self.filters, self.analysis_method, after_key).query()
-        index = self.settings["index"][self.index()]
+    def get_activity_days(self, total_response):
+        """
+        Returns total number of activity days. Handles getting the total number, even if the number is > 10k
+        :param total_response: The total object from the elasticsearch JSON response
+        :return: <int> Total number of activity days
+        """
 
-        return self.es.search(index = index, body = query)
+        if total_response['relation'] == 'eq':
+            return total_response['value']
+        else:
+            query = self.get_elasticsearch_query()
+            return self.es.count(index=self.index, body=query)['count']
+
+    def get_elasticsearch_response(self, after_key=None, deposits=False):
+        query = self.get_elasticsearch_query(after_key, deposits)
+        return self.es.search(index=self.index, body = query)
+
+    def get_elasticsearch_query(self,after_key=None, deposits=False):
+        return QueryBuilderFactory(deposits = deposits).get(self.filters, self.analysis_method, after_key).query()
 
     def get_title(self):
         return NotImplementedError
