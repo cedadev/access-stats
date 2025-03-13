ff57fa3b5ad23ac25f5bc5f5f125367b53ee6ab4
Edbo849 <edborthwick84@gmail.com>
2025-03-13 14:23:47 +0000

    Updated url naming patterns due to django 4.2's stricter URL resolution

--- downloads/urls.py
+++ downloads/urls.py
@@ -4,8 +4,8 @@ from downloads.views import IndexView, JsonView, TxtView, CsvView, XlsxView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="downloads"),
-    path("json/<analysis_method>", JsonView.as_view(), name="downloads/json"),
-    path("txt/<analysis_method>", TxtView.as_view(), name="downloads/txt"),
-    path("csv/<analysis_method>", CsvView.as_view(), name="downloads/csv"),
-    path("xlsx/<analysis_method>", XlsxView.as_view(), name="downloads/xlsx"),
+    path("json/<analysis_method>", JsonView.as_view(), name="downloads_json"),
+    path("txt/<analysis_method>", TxtView.as_view(), name="downloads_txt"),
+    path("csv/<analysis_method>", CsvView.as_view(), name="downloads_csv"),
+    path("xlsx/<analysis_method>", XlsxView.as_view(), name="downloads_xlsx"),
 ]
