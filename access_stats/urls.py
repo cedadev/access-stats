45e5e76831946b61cb0bbeb8d6c6262d1296999f
Edbo849 <edborthwick84@gmail.com>
2025-03-21 15:31:16 +0000

    Added health url

--- access_stats/urls.py
+++ access_stats/urls.py
@@ -1,15 +1,20 @@
+from django.http import HttpResponse
 from django.urls import path, include
 from django.conf import settings
 
 from access_stats.views import IndexView
 
+
+def health_view(request):
+    return HttpResponse("OK")
+
+
 if settings.PUBLIC_SITE:
-    urlpatterns = [
-        path("", include("downloads.urls"))
-    ]
+    urlpatterns = [path("", include("downloads.urls"))]
 else:
     urlpatterns = [
         path("", IndexView.as_view(), name="index"),
         path("downloads/", include("downloads.urls")),
         path("deposits/", include("deposits.urls")),
+        path("health/", health_view, name="health"),
     ]
