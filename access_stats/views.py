556832b5cd1b068f4352ea501b7464eefe5fbeea
m-haines <matthew.r.haines@gmail.com>
2025-03-27 15:10:06 +0000

    Updated view.py in access_stats and downloads to point at index.html directly, to see if that helps Django find the file at runtime.

--- access_stats/views.py
+++ access_stats/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    template_name = "access_stats/templates/index.html"
     
     def get(self, request):
         
