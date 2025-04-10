652020fbaf250da7bfa93d28c5b943584fd7a695
m-haines <matthew.r.haines@gmail.com>
2025-04-10 11:53:20 +0100

    Initial start of work on bot filtering.

--- downloads/forms.py
+++ downloads/forms.py
@@ -33,3 +33,12 @@ class FilterForm(DateForm):
         label="User type:",
         widget=forms.Select(attrs={"class": "form-control", "placeholder": ""}),
     )
+    bots = forms.ChoiceField(
+        choices=[
+            ("remove-bots", "Bots filtered"),
+            ("allow-bots", "Bots allowed"),
+        ],
+        required=False,
+        label="Bot type:",
+        widget=forms.Select(attrs={"class": "form-control", "placeholder": ""}),
+    )
